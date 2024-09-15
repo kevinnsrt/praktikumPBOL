687c4c734133c2fde394875f65b1d98f5a594562
Kevin Sirait <151734589+kevinnsrt@users.noreply.github.com>
2024-09-15 13:03:36 +0700

    Tugas1

---
+++ Tugas1/Tugas1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Tugas1 {
+ public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Masukkan Nama: ");
+    String nama= input.nextLine();
+    System.out.print("Masukkan NIM: ");
+    String nim = input.nextLine();
+   System.out.println("-----------------------------------");
+    System.out.println("Nama: "+nama);
+    System.out.println("NIM: "+nim);
+ }   
+}
